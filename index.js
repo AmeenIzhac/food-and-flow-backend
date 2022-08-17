228cd1eb91ac826eb1fd5c91b0652d448292f19f
ameen <ameen.izhac@gmail.com>
2022-08-17 15:11:49 +0100

    added a line

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(bodyParser.json());
 app.use('/', routeHandler);
 
 const port = process.env.PORT || 4000;
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}.`);
 });
